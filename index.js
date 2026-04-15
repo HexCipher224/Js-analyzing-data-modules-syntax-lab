2f674f20a3b2e74a0f7fa005968b3ef36de71762
HexCipher224 <mofiamadiback@gmail.com>
2026-04-15 04:30:09 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,4 +1,21 @@
+//step 1: combineUsers function creation
+function combineUsers(...args) {
+  //step 2:initialize return object
+  const combinedObject = {
+    users: []
+  };
 
+  //step 3and 4: loop through each argument and merge arrays
+  args.forEach(element => {
+    combinedObject.users.push(...arr);
+  });
+
+  //step 5: add today's date in M/d/yyyy format
+  combinedObject.merge_date = new Date().toString("M/d/yyyy");
+}
+
+  // step 7: retuen object
+  return combinedObject;
 
 
 module.exports = {
